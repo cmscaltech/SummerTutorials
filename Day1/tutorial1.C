bad9fe00a32ee0e19f6d38a6472ad6a084a393c9
jaylawhorn <jay@jaylawhorn.com>
2016-04-28 16:17:31 -0700

    shell for tutorial 1

---
+++ Day1/tutorial1.C
@@ -0,0 +1,21 @@
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include <TROOT.h>                        // access to gROOT, entry point to ROOT system
+#include <TSystem.h>                      // interface to OS
+#include <TFile.h>                        // file handle class
+#include <TTree.h>                        // class to access ntuples
+#include <TH1F.h>                         // histograms containing floats
+#include <TCanvas.h>                      // class for ROOT drawing canvases
+#include <vector>                         // STL vector class
+#include <iostream>                       // standard I/O
+#include <iomanip>                        // functions to format standard I/O
+#include <fstream>                        // functions for file I/O
+#include <string>                         // C++ string class
+#include <sstream>                        // class for parsing strings
+#endif
+
+void tutorial1() {
+
+
+
+
+}
